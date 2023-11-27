6f08f0baf755a715d1846025239ef00a889fa892
avergara0011 <140130089+avergara0011@users.noreply.github.com>
2023-11-27 00:56:50 -0800

    Home fragment now displays map
    Implemented Maps API, having trouble displaying only CSUN, and displaying markers for parking lots

--- app/src/main/java/com/example/parkwise/HomeFragment.java
+++ app/src/main/java/com/example/parkwise/HomeFragment.java
@@ -2,26 +2,33 @@ package com.example.parkwise;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
-    private ImageButton mainButton;
-    private ImageButton homeButton;
-    private ImageButton accButton;
-
-    private ImageButton setButton;
-    private boolean lock;
+    private GoogleMap map;
+    private SupportMapFragment mapFragment;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -71,8 +78,71 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used
+        mapFragment = SupportMapFragment.newInstance();
+        getChildFragmentManager().beginTransaction().replace(R.id.mapContainer, mapFragment).commit();
 
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        // Add markers for each parking lot
+        LatLng b5LatLng = new LatLng(34.241594, -118.532754);
+        addParkingLotMarker(b5LatLng, "B5", 50); // Add more similarly.
+
+        map.setOnMarkerClickListener(marker -> {
+            // Display parking lot details in window
+            showLotInfo(marker);
+            return false;
+        });
+
+        // Setting coords to focus on CSUN campus using CameraUpdate
+        LatLng csunLatLng = new LatLng(34.2419, -118.5283); // CSUN's approximate center coordinates
+        float zoomLevel = 16f; // Adjust the zoom level as needed
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(csunLatLng, zoomLevel);
+        map.animateCamera(cameraUpdate);
+    }
+
+    // method for adding parking lot markers
+    private void addParkingLotMarker(LatLng position, String lotName, int availableStalls) {
+        MarkerOptions markerOptions = new MarkerOptions().position(position).title("Parking Lot " + lotName);
+        map.addMarker(markerOptions).setTag(new ParkingLotDetails(lotName, availableStalls));
+    }
+
+    private void showLotInfo(Marker marker) {
+        ParkingLotDetails details = (ParkingLotDetails) marker.getTag();
+        if (details != null) {
+            // Display details in a custom info window or dialog
+            // For example, use a custom layout or AlertDialog to show lot number and available stalls
+            String lotName = details.getLotName();
+            int availableStalls = details.getAvailableStalls();
+
+            // Example: Use a Toast to display details
+            Toast.makeText(requireContext(), "Lot " + lotName + " - Available Stalls: " + availableStalls, Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
+
+// Class to hold parking lot details
+class ParkingLotDetails {
+    private String lotName;
+    private int availableStalls;
+
+    ParkingLotDetails(String lotName, int availableStalls) {
+        this.lotName = lotName;
+        this.availableStalls = availableStalls;
+    }
+
+    String getLotName() {
+        return lotName;
+    }
+
+    int getAvailableStalls() {
+        return availableStalls;
+    }
 }
