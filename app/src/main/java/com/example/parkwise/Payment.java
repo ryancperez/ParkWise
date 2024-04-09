6f0f1951effa21152586e894302dfe2a1ab2a9fe
KeeperSev <sevada.ghazaryan.562@my.csun.edu>
2024-04-09 10:18:23 -0700

    Updated payment system

--- app/src/main/java/com/example/parkwise/Payment.java
+++ app/src/main/java/com/example/parkwise/Payment.java
@@ -5,12 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+
 
 import java.sql.Connection;
 import java.sql.SQLException;
